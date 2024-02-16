c00789064d16f369ebc1deca75fb6acfec95e375
Luis Lescano <luislescanologo@hotmail.com>
2024-02-16 15:04:04 -0300

    menu 15-2-24

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Intro from './components/Intro'
 import Work from './components/Work'
 import About from './components/About'
@@ -9,24 +9,46 @@ import NavBar from './components/NavBar'
 import './App.css'
 
 function App() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
 
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
 
   return (
-    <div className='h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden'>
-      <NavBar />
-      <div className=' snap-center'>
-        <Intro />
+    <Router>
+      <div className='h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden'>
+        <NavBar />
+        {/* <ScrollToTop /> */}
+        {/* <div className='snap-container'> */}
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        {/* </div> */}
       </div>
-      <div className=' snap-center'>
-        <Work />
-      </div>
-      <div className=' snap-center'>
-        <About />
-      </div>
-      <div className=' snap-center'>
-        <Contact />
-      </div>
-    </div>
+    </Router>
+    // <div className=' h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden'>
+    //   <NavBar />
+    //   <div className=' snap-center'>
+    //     <Intro />
+    //   </div>
+    //   <div className=' snap-center'>
+    //     <Work />
+    //   </div>
+    //   <div className=' snap-center'>
+    //     <About />
+    //   </div>
+    //   <div className=' snap-center'>
+    //     <Contact />
+    //   </div>
+    // </div>
   )
 }
 
