b845959c0f59b4850dea12888fbd30158dba1a5b
Luis Lescano <luislescanologo@hotmail.com>
2024-02-23 11:32:11 -0300

    spyNavBar done!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,32 @@
-
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
 import Intro from './components/Intro'
 import Work from './components/Work'
 import About from './components/About'
 import Contact from './components/Contact'
-import NavBar from './components/NavBar'
 import './App.css'
+import NavBar from './components/NavBar';
 
 function App() {
-  function ScrollToTop() {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return null;
-  }
+  const [currentSection, setCurrentSection] = useState('home');
 
   return (
-    <Router>
-      <div className='h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden'>
-        <NavBar />
-        {/* <ScrollToTop /> */}
-        {/* <div className='snap-container'> */}
-          <Routes>
-            <Route path="/" element={<Intro />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        {/* </div> */}
+    <div className=' App flex flex-col'>
+      <div className='snap-y snap-mandatory h-screen overflow-x-hidden '>
+        <div id='home' className=' snap-center' onMouseOver={() => setCurrentSection('home')}>
+          <Intro />
+        </div>
+        <div id='work' className=' snap-center' onMouseOver={() => setCurrentSection('work')}>
+          <Work />
+        </div>
+        <div id='about' className=' snap-center' onMouseOver={() => setCurrentSection('about')}>
+          <About />
+        </div>
+        <div id='contact' className=' snap-center' onMouseOver={() => setCurrentSection('contact')}>
+          <Contact />
+        </div>
       </div>
-    </Router>
-    // <div className=' h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden'>
-    //   <NavBar />
-    //   <div className=' snap-center'>
-    //     <Intro />
-    //   </div>
-    //   <div className=' snap-center'>
-    //     <Work />
-    //   </div>
-    //   <div className=' snap-center'>
-    //     <About />
-    //   </div>
-    //   <div className=' snap-center'>
-    //     <Contact />
-    //   </div>
-    // </div>
+      <NavBar currentSection={currentSection} setCurrentSection={setCurrentSection} />
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,59 @@
-import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
+import React, {  useEffect, useState } from 'react'
 
 
-const NavBar = () => {
-    const location = useLocation();
+const NavBar = ({currentSection, setCurrentSection}) => {
+    // const [targetElement, setTargetElement] = useState('');
+    
+    // const handleClick = (e) => {
+    //     e.preventDefault();
+    //     const targetId = e.target.getAttribute('href').slice(1);
+    //     const element = document.getElementById(targetId);
+    //     if (targetElement) {
+    //         element.scrollIntoView({ behavior: 'smooth' });
+    //         setTargetElement(element.id);
+    //         currentSection = targetElement
+    //         console.log(currentSection)
+    //     }
+    // };
+    
+    // useEffect(() => {
+    //     console.log(targetElement)
+    // }, []);
 
-
+    const handleClick = (e, section) => {
+        // e.preventDefault();
+        console.log('1')
+        setCurrentSection(section);
+      };
+    
     return (
         <div className=' fixed right-0 bottom-0 mb-20 mr-16'>
-            <ul className=' w-20'>
-                <li className={`${location.pathname === '/' ? 'font-extrabold' : ''} transition-all`}>
-                    <NavLink to='/'>
-                        Home
-                    </NavLink>
-                </li>
-                <li className={`${location.pathname === '/work' ? 'font-extrabold' : ''} transition-all`}>
-                    <NavLink to='work'>
-                        Work
-                    </NavLink>
-                </li>
-                <li className={`${location.pathname === '/about' ? 'font-extrabold' : ''} transition-all`}>
-                    <NavLink to='about'>
-                        About me
-                    </NavLink>
-                </li>
-                <li className={`${location.pathname === '/contact' ? 'font-extrabold' : ''} transition-all`}>
-                    <NavLink to='contact'>
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
+            <nav className=' w-20 flex-col flex'>
+                <a className={` w-20 ${currentSection === 'home' ? 'font-bold' : ''} transition-all hover:font-bold`}
+                    href='#home'
+                    onClick={(e) => handleClick(e, 'home')}
+                >
+                    Home
+                </a>
+                <a className={` w-20 ${currentSection === 'work' ? 'font-bold' : ''} transition-all hover:font-bold`}
+                    href='#work'
+                    onClick={(e) => handleClick(e, 'work')}
+                >
+                    Work
+                </a>
+                <a className={` w-20 ${currentSection === 'about' ? 'font-bold' : ''} transition-all hover:font-bold`}
+                    href='#about'
+                    onClick={(e) => handleClick(e, 'about')}
+                >
+                    About me
+                </a>
+                <a className={` w-20 ${currentSection === 'contact' ? 'font-bold' : ''} transition-all hover:font-bold`}
+                    href='#contact'
+                    onClick={(e) => handleClick(e, 'contact')}
+                >
+                    Contact
+                </a>
+            </nav>
         </div>
     )
 }
