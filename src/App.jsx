93f2a3f2fa7a434cbac8e2b6e0726194f306e86e
Luis Lescano <luislescanologo@hotmail.com>
2024-04-12 17:41:14 -0300

    resume url fix3

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import CVLuis from './views/CVLuis';
 import ProvResume from './views/ProvResume';
-// import Intro from './components/Intro'
-// import Work from './components/Work'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import './App.css'
-// import NavBar from './components/NavBar';
 
 function App() {
-  // const [currentSection, setCurrentSection] = useState('home');
-  // const [currentLighted, setCurrentLighted] = useState(false);
 
   return (
     <div className=' cursor-default'>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="https://portfolio3-ruddy.vercel.app//resume" element={<ProvResume/>} />
+        <Route path="https://portfolio3-ruddy.vercel.app/resume" element={<ProvResume/>} />
         <Route path="/curriculum" element={<CVLuis/>} />
       </Routes>
     </div>
-    // <div className=' App flex flex-col'>
-    //   <div className='snap-y snap-mandatory h-screen overflow-x-hidden '>
-    //     <div id='home' className=' snap-start ' onMouseEnter={() => setCurrentSection('home')}>
-    //       <Intro
-    //         currentLighted={currentLighted} setCurrentLighted={setCurrentLighted}
-    //       />
-    //     </div>
-    //     <div id='work' className=' snap-start ' onMouseEnter={() => setCurrentSection('work')}>
-    //       <Work />
-    //     </div>
-    //     <div id='about' className=' snap-start' onMouseEnter={() => setCurrentSection('about')}>
-    //       <About />
-    //     </div>
-    //     <div id='contact' className=' snap-start' onMouseEnter={() => setCurrentSection('contact')}>
-    //       <Contact />
-    //     </div>
-    //   </div>
-    //   <NavBar
-    //     currentSection={currentSection} setCurrentSection={setCurrentSection}
-    //     currentLighted={currentLighted} setCurrentLighted={setCurrentLighted}
-    //   />
-    // </div>
   )
 }
 
